1c1dd9af7b97fa6a254c3ade66cc70ec37b7cff4
Serena Savarese <sesesa02@gmail.com>
2024-11-21 14:52:50 +0100

    Nuove animazioni

--- script.js
+++ script.js
@@ -25,7 +25,14 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+            // active section for animation on scroll 
+            sec.classList.add('show-animate');
         }
+        // if you want to usa animation that repeats on scroll use this 
+        else{
+            sec.classList.remove('show-animate');
+        }
+
     });
 
     // sticky header
